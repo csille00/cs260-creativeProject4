5f32891a48c8cbd67e1a9d22b319c9a7fdfe05e6
garett-whimple <74328475+garett-whimple@users.noreply.github.com>
2022-11-23 16:41:51 -0700

    moved back end function into one file

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParse = require('body-parser')
 const myMongo = require('./mongo')
+const ratings = require('@mtucourses/rate-my-professors').default
+const mongoose = require("mongoose");
+const { stringify } = require("querystring");
 
 const app = express()
 app.use(express.static('public'))
@@ -38,3 +41,66 @@ app.delete('/api/teacher/:name', async (req, res) => {
         res.sendStatus(500)
     }
 })
+
+app.get('/api/teacher/:firstName/:lastName', async (req, res) => {
+    console.log('In RMP API call')
+    const firstName = req.params.firstName
+    const lastName = req.params.lastName
+    const name = `${firstName} ${lastName}`
+    await getProfessorData(name)
+
+})
+
+
+//MONGO DATABASE CALLS 
+
+//TODO: mongoose.connect
+
+const teacherSchema = mongoose.schema({
+    name: String,
+    averageDifficulty: Number,
+    averageRating: Number,
+    numRatings: Number,
+    wouldTakeAgain: Number,
+    priority: Number
+})
+const teacherModel = mongoose.model('Teacher', teacherSchema)
+
+async function addTeacher(data) {
+    const newTeacher = new teacherModel({
+        name: data.name,
+        averageDifficulty: data.averageDifficulty,
+        averageRating: data.averageRating,
+        numRatings: data.numRatings,
+        wouldTakeAgain: data.wouldTakeAgain,
+        priority: data.priority
+    })
+
+    await newTeacher.save();
+
+}
+
+async function deleteTeacher(nameToDelete) {
+    await teacherModel.deleteOne({
+        name: nameToDelete
+    })
+}
+
+async function getTeacher(){
+    let teachers = await teacherModel.find();
+    console.log(teachers)
+    return teachers
+}
+
+
+//RMP API call
+getProfessorData = async function getProfessorData(name) {
+    const teacher = await ratings.searchTeacher(name, 'U2Nob29sLTEzNQ==')
+    if (teacher.length === 0) {
+        return {'response': 'No available data for that name'}
+    }
+    else {
+        const teacherInfo = await ratings.getTeacher(teacher[0].id)
+        return {avgRating: teacherInfo.avgRating, avgDifficulty: teacherInfo.avgDifficulty, numRatings: teacherInfo.numRatings, wouldTakeAgainPercent: teacherInfo.wouldTakeAgainPercent}
+    }
+}
