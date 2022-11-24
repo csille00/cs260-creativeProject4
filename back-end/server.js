cbaf8dcfda0481c9f2a772b32f0cb22ad7e10010
Ubuntu <ubuntu@ip-172-31-5-134.us-west-1.compute.internal>
2022-11-24 01:53:50 +0000

    Actually finished the back-end

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParse = require('body-parser')
-const myMongo = require('./mongo')
 const ratings = require('@mtucourses/rate-my-professors').default
 const mongoose = require("mongoose");
 const { stringify } = require("querystring");
@@ -13,28 +12,52 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "POST, GET, DELETE")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()    
 })
 
 let teacherObjectList = []
 
 app.get('/api/teacher', async (req, res) => {
-    const listOfTeacherObjects = await myMongo.getTeacher()
+    console.log('In get teacher')
+    const listOfTeacherObjects = await getTeacher()
     res.send(listOfTeacherObjects)
 })
 
-app.put('/api/teacher/:name/:priority', async (req, res) => {
-    console.log('In update priority')
-    const firstName = req.params.name
-    const priority = req.params.piority
-    
-    
+app.post('/api/teacher/:firstName/:lastName', async (req, res) => {
+    console.log('In post teacher')
+    try {
+        const firstName = req.params.firstName
+        const lastName = req.params.lastName
+        const name = `${firstName} ${lastName}`
+        const data = await getProfessorData(name)
+        data.priority = 0
+        data.name = name
+        addTeacher(data)
+        res.sendStatus(200)
+    } catch {
+        console.log('Unable to add teacher')
+        res.sendStatus(400)
+    }
 })
 
-app.delete('/api/teacher/:name', async (req, res) => {
-    console.log('In delete')
-    const name = req.params.name
+app.put('/api/teacher/:id/:priority', async (req, res) => {
+    console.log('In update priority')
     try {
-        await myMongo.deleteTeacher(name)
+        const id = req.params.id
+        const priority = req.params.priority
+        await updateTeacher(id, priority)
+        res.sendStatus(200)
+    } catch {
+        console.log('Unable to update teacher data')
+        res.sendStatus(400)
+    }
+})
+
+app.delete('/api/teacher/:id', async (req, res) => {
+    console.log('In delete')
+    const id = req.params.id
+    try {
+        await deleteTeacher(id)
         res.sendStatus(200)
     } catch (e) {
         console.log(e)
@@ -42,21 +65,23 @@ app.delete('/api/teacher/:name', async (req, res) => {
     }
 })
 
-app.get('/api/teacher/:firstName/:lastName', async (req, res) => {
-    console.log('In RMP API call')
-    const firstName = req.params.firstName
-    const lastName = req.params.lastName
-    const name = `${firstName} ${lastName}`
-    await getProfessorData(name)
-
-})
+// app.get('/api/teacher/:firstName/:lastName', async (req, res) => {
+//     console.log('In RMP API call')
+//     const firstName = req.params.firstName
+//     const lastName = req.params.lastName
+//     const name = `${firstName} ${lastName}`
+//     await getProfessorData(name)
+// })
 
 
 //MONGO DATABASE CALLS 
 
-//TODO: mongoose.connect
+mongoose.connect('mongodb://localhost:27017/teacher', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
-const teacherSchema = mongoose.schema({
+const teacherSchema = mongoose.Schema({
     name: String,
     averageDifficulty: Number,
     averageRating: Number,
@@ -69,10 +94,10 @@ const teacherModel = mongoose.model('Teacher', teacherSchema)
 async function addTeacher(data) {
     const newTeacher = new teacherModel({
         name: data.name,
-        averageDifficulty: data.averageDifficulty,
-        averageRating: data.averageRating,
+        averageDifficulty: data.avgDifficulty,
+        averageRating: data.avgRating,
         numRatings: data.numRatings,
-        wouldTakeAgain: data.wouldTakeAgain,
+        wouldTakeAgain: data.wouldTakeAgainPercent,
         priority: data.priority
     })
 
@@ -80,10 +105,15 @@ async function addTeacher(data) {
 
 }
 
-async function deleteTeacher(nameToDelete) {
+async function deleteTeacher(id) {
     await teacherModel.deleteOne({
-        name: nameToDelete
+        _id: id
     })
+}
+
+async function updateTeacher(id, priority) {
+    const mypriority = parseInt(priority)
+    await teacherModel.updateOne({ _id:id }, { $set: {priority: mypriority}})
 }
 
 async function getTeacher(){
@@ -94,7 +124,7 @@ async function getTeacher(){
 
 
 //RMP API call
-getProfessorData = async function getProfessorData(name) {
+async function getProfessorData(name) {
     const teacher = await ratings.searchTeacher(name, 'U2Nob29sLTEzNQ==')
     if (teacher.length === 0) {
         return {'response': 'No available data for that name'}
@@ -104,3 +134,5 @@ getProfessorData = async function getProfessorData(name) {
         return {avgRating: teacherInfo.avgRating, avgDifficulty: teacherInfo.avgDifficulty, numRatings: teacherInfo.numRatings, wouldTakeAgainPercent: teacherInfo.wouldTakeAgainPercent}
     }
 }
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
